8a6693c7207af844c7c781f33ee1b0b8f1abeb26
vukhoi2k5 <vukhoi353@gmail.com>
2024-10-07 00:44:30 +0700

    update delete

--- routes/admin/adminRouter.js
+++ routes/admin/adminRouter.js
@@ -7,6 +7,9 @@ routers.get("/test",(req,res)=>{
 routers.get("/list",async (req,res)=>{
     await adminController.getAllArticle(req,res)
 })
+routers.post("/list/sreach", async (req,res)=>{
+    await adminController.getArticleByField(req,res)
+})
 routers.get("/create1",async(req,res)=>{
     res.render('admin/create',{message : ""});})
 
@@ -14,5 +17,14 @@ routers.post('/create-post',async (req,res)=>{
     // res.send(req.body)
     await adminController.createArticle(req,res)
 })
+
+routers.put("/update/:id",async(req,res)=>{
+    await adminController.updateArticle(req,res)
+})
+
+routers.delete("/delete/:id",async(req,res)=>{
+    await adminController.deleteArticle(req,res)
+})
+
 
 module.exports = routers
